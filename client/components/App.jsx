dc67cf62ae5c3bd6e51fa562913445253dc00c36
zubair-akbar <62559732+zubair-akbar@users.noreply.github.com>
2021-05-19 02:34:11 -0400

    Updates to main page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 
 function App() {
 
+	let handleGitClick = () => {
+		window.open('https://github.com/zubair-akbar');
+	};
+
 	return (
 		<div>
 			<MyVideo autoPlay loop muted>
@@ -12,8 +16,13 @@ function App() {
 				<Header>VERTEX Route</Header>
 				<Para>An app that makes it a little easier to ascend...</Para>
 				<ButtonWrap>
-					<Button>How it works</Button>
-					<Button>View on Github</Button>
+					<Button>
+            How it works
+					</Button>
+					<Button
+						onClick={handleGitClick}>
+            View on Github
+					</Button>
 				</ButtonWrap>
 			</Wrapper>
 		</div>
@@ -51,17 +60,31 @@ display: flex;
 flex-direction: row;
 `;
 
-const Button = styled.a`
+const Button = styled.button`
   border: 2px solid rgba(255,255,255,0.20);
   border-radius: 61px;
   font-size: 15px;
   color: #FFFFFF;
+  background-color: transparent;
   letter-spacing: 1px;
   text-decoration: none;
   text-transform: uppercase;
   padding: 10px 25px;
   display: inline-block;
   margin-right: 15px;
+  transition:all 2.2s ease-in-out;
+  &:hover {
+    text-shadow:
+    0 0 10px rgba(255,255,255, 1),
+    0 0 50px rgba(255, 255, 255, .8),
+    0 0 75px rgba(255, 255, 255, .6),
+    0 0 76px rgba(255, 255, 255, .4),
+    0 0 77px rgba(255, 255, 255, .5),
+    0 0 78px rgba(255, 255, 255, .4),
+    0 0 79px rgba(255, 255, 255, .3),
+    0 0 80px rgba(255, 255, 255, .2),
+    0 0 85px rgba(255, 255, 255, .1);
+  }
   `;
 
 const MyVideo = styled.video`
